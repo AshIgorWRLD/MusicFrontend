25a2b6440e8b6f55f993e88033bfa1cd29e792e3
AshIgorWRLD <ashigorwrld@gmail.com>
2022-06-01 04:09:35 +0700

    add list with inserts

--- music-studio-front/src/App.js
+++ music-studio-front/src/App.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, {useMemo, useState} from 'react'
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/my_modal/MyModal";
+import MyButton from "./components/UI/buttons/MyButton";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Kanye West", body: "Genius"},
+        {id: 2, title: "Drake", body: "Man"},
+        {id: 3, title: "Juice Wrld", body: "Legend"}
+    ])
+
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts;
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.includes(filter.query))
+    }, [filter.query, sortedPosts])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Add rapper
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Best rappers"/>
+
+        </div>
+    );
 }
 
 export default App;
--- music-studio-front/src/index.js
+++ music-studio-front/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <div>
-        In the night I hear em talk
-    </div>,
+    <App/>,
     document.getElementById('root'));
 
